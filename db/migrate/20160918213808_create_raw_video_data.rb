1cb996c862ce65dd226ee586d1634befa4e59748
SputterPuttRedux <dstanfield.esq@gmail.com>
2016-09-18 18:06:36 -0400

    add columns to migration file

--- db/migrate/20160918213808_create_raw_video_data.rb
+++ db/migrate/20160918213808_create_raw_video_data.rb
@@ -1,6 +1,13 @@
-class CreateRawVideoData < ActiveRecord::Migration
+class CreateRawVideoDatas < ActiveRecord::Migration
   def change
-    create_table :raw_video_data do |t|
+    create_table :raw_video_datas do |t|
+      t.page_info_id :string
+      t.derived_video_id :string
+      t.etag :string
+      t.title :string
+      t.description :string
+      t.published_date :date
+      t.timestamps
     end
   end
 end
