b5ef8e9f6fdf21c5e7175a1ef2594787cfc7fec9
SputterPuttRedux <dstanfield.esq@gmail.com>
2016-09-18 17:13:32 -0400

    enable assets. comment unnecessary config

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module YoutubeDefenderMap
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.action_mailer.perform_caching = true
+    config.assets.enabled = true
   end
 end
