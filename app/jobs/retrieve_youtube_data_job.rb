a421b4e308125ffcc9fc79c242b5a70f046b36bc
SputterPuttRedux <dstanfield.esq@gmail.com>
2016-09-19 21:47:29 -0400

    [WIP] need to refactor. Create class to retrieve info from YoutTube api

--- app/jobs/retrieve_youtube_data_job.rb
+++ app/jobs/retrieve_youtube_data_job.rb
@@ -10,31 +10,35 @@ class RetrieveYoutubeDataJob < ActiveJob::Base
   private
 
   def persist(data)
-    channel_info = channel_info(data)
+    channel_info(data)
 
-    channel_info.raw_video_datas.create(
-      raw_video_data(data)
-    )
+    raw_video_data(data).map do |rvd|
+      ChannelInfo.where(etag: data['etag']).first.raw_video_datas \
+        .create(rvd)
+    end
   end
 
   def raw_video_data(data)
-    data[:items].map do |item|
-      RawVideoData.create_with(
-        etag: item[:etag],
-        title: item[:snippet][:title]
-        description: item[:snippet][:description]
-        published_date: item[:snippet][:publishedAt]
+    data['items'].map do |item|
+      rvd = RawVideoData.create_with(
+        etag: item['etag'],
+        title: item['snippet']['title'],
+        description: item['snippet']['description'],
+        published_date: item['snippet']['publishedAt']
       ).find_or_create_by(
-        derived_video_id: item[:id][:videoId]
+        derived_video_id: item['id']['videoId']
       )
+      # rvd.save ? Rails.logger.info { "Created RawVideoData id: #{rvd.id}" } : Rails.logger.warn { "RawVideoData #{rvd.errors}" }
     end
   end
 
   def channel_info(data)
-    ChannelInfo.create(
-      total_results: data[:pageInfo][:totalResults],
-      etag: results[:etag],
-      derived_channel_id: data[:items].first[:snippet][:channelId]
+    ci = ChannelInfo.create(
+      total_results: data['pageInfo']['totalResults'],
+      etag: data['etag'],
+      derived_channel_id: data['items'].first['snippet']['channelId']
     )
+
+    # ci.save ? Rails.logger.info { "Created ChannelInfo #{ci.id}" } : Rails.logger.warn {"ChannelInfo NOT saved! #{rvd.errors}"}
   end
 end
