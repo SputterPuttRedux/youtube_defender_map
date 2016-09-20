2c7803f5f9586032f90f25920a95f6b53d6c70d9
SputterPuttRedux <dstanfield.esq@gmail.com>
2016-09-19 21:43:18 -0400

    channel info migration

--- db/migrate/20160919000454_create_channel_infos.rb
+++ db/migrate/20160919000454_create_channel_infos.rb
@@ -1,6 +1,6 @@
-class CreatePageInfos < ActiveRecord::Migration
+class CreateChannelInfos < ActiveRecord::Migration
   def change
-    create_table :page_infos do |t|
+    create_table :channel_infos do |t|
       t.integer :total_results
       t.string :etag
       t.string :derived_channel_id
